bf288dbd56dc4d2f204449d77f3d7dcc267f5df7
Ingi Thor Sigurdsson <ingisigurds@gmail.com>
2018-12-10 11:14:35 +0000

    Added templates

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
-app.debug = True
-
 @app.route('/')
-def hello_world():
-    return 'Hello World'
+def index():
+    return render_template('index.html')
+
+@app.route('/about')
+def about():
+    return 'The about page'
+
 
 if __name__ == '__main__':
-	app.run()
+    app.run(debug=True)
