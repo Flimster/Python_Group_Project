826c5b26d60725fa874b70c41e7e249d2c39868b
Ingi Thor Sigurdsson <ingisigurds@gmail.com>
2018-12-10 11:52:19 +0000

    The user can now upload files to the server

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
-from flask import Flask, render_template
+from pathlib import Path
+from flask import Flask, render_template, request
+
 app = Flask(__name__)
 
 @app.route('/')
@@ -9,6 +11,17 @@ def index():
 def about():
     return 'The about page'
 
+@app.route('/file', methods=['POST'])
+def upload_file():
+	if request.method == 'POST':
+		p = Path('.')
+		print('The method was a post method')
+		tmp = request.files['myfile'].read().decode()
+		print(type(tmp))
+		for file in p.glob('./tests/impl.py'):
+			with open(file, 'w') as f:
+				f.write(tmp)
+	return "Success"
 
 if __name__ == '__main__':
     app.run(debug=True)
