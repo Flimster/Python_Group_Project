1e2a64c0f9a2d5148bf790f8e4fdb12489d45a68
Ingi Thor Sigurdsson <ingisigurds@gmail.com>
2018-12-10 16:15:03 +0000

    Created routes for creating problems and assignments

--- thinkback/views/assignments.py
+++ thinkback/views/assignments.py
@@ -1,19 +1,26 @@
 # thinkback/views/assignments.py
 
 from flask import Blueprint
-from ..models import Project
+from ..models import Project, Problem
 
 assignments_blueprint = Blueprint('/assignments', __name__)
 
 # Create a project with 10 empty problems
-project1 = Project()
+project1 = Project("Verkefni 1")
 for i in range(10):
-	project1.create_problem()
+	project1.create_problem("Tmp", "This is description")
 
 @assignments_blueprint.route('/<assignment>', methods=['GET'])
 def get_assignments(assignment):
 	return "Hello world this is assignment {}".format(assignment)
 
+@assignments_blueprint.route('/createAssignment')
+def create_assignment():
+	return "Here you create the assignments {}"
+
+@assignments_blueprint.route('/<assignment>/create_problem')
+def create_problem(assignment):
+	return "Here you create problem for assignment {}".format(assignment)
 
 @assignments_blueprint.route('/<assignment>/<problem>', methods=['GET'])
 def problem_description(assignment, problem):
