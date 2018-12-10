1839840a2c9ddee55d4cdeeee0b067660b3e942b
Ingi Thor Sigurdsson <ingisigurds@gmail.com>
2018-12-10 12:43:51 +0000

    Changed the folder structure

---
+++ thinkback/__init__.py
@@ -0,0 +1,34 @@
+from pathlib import Path
+from subprocess import call
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('/index.html')
+
+@app.route('/about')
+def about():
+    return 'This small project was made by two computer science studenst in the University of Reykjavik'
+
+@app.route('/problem/<problem_name>', methods=['GET'])
+def problem_description(problem_name):
+	return "Hello world this is problem {}".format(problem_name)
+
+@app.route('/upload/<problem_name>', methods=['POST'])
+def upload_sum_problem(problem_name):
+	if request.method == 'POST':
+		p = Path('.')
+		file_uploaded = request.files['myfile'].read().decode()
+		for file in p.glob('./tests/impl.py'):
+			with open(file, 'w') as f:
+				f.write(file_uploaded)
+	call(["pytest", "-q"]) 
+	return "Success"
+
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
