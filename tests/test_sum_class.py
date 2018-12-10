9895217bf28929e0d5f160e5a46f7d18e9d39535
Ingi Thor Sigurdsson <ingisigurds@gmail.com>
2018-12-10 10:25:59 +0000

    Added some changes to the tests

--- tests/test_sum_class.py
+++ tests/test_sum_class.py
@@ -5,8 +5,8 @@ class TestSum:
 
 	def test_one_two(self):
 		f = impl.sum(1, 2)
-		assert f == 3
+		assert f == 3, "Expected output: 3. Optained output: {}".format(f)
 
-	def test_one_three(self):
-		f = impl.sum(1, 3)
-		assert f == 3
+	def test_one_four(self):
+		f = impl.sum(1, 4)
+		assert f == 5, "Expected output: 5. Optained output: {}".format(f)
