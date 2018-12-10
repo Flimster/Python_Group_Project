b18c351c2a1b06964f0b7258af3c8238f47ee42d
Ingi Thor Sigurdsson <ingisigurds@gmail.com>
2018-12-10 16:14:39 +0000

    Added some changes to models.py

--- thinkback/models.py
+++ thinkback/models.py
@@ -1,15 +1,15 @@
+class Project:
+	def __init__(self, name):
+		self.project_name = name
+		self.problem_list = []
+		self.problem_count = 0
+
+	def create_problem(self, name, description):
+		self.problem_list.append(Problem(name, description))
+		self.problem_count += 1
+
+
 class Problem:
 	def __init__(self, name, description):
 		self.name = name
 		self.description = description
-	
-
-class Project:
-	def __init__(self):
-		self.problem_list = []
-		self.counter = 0
-
-	def create_problem(self):
-		name = str(self.counter)
-		self.problem_list.append(Problem(name, "This is a problem"))
-		self.counter += 1
