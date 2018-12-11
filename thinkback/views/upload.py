5cd63a4c93b2b3d88ff95814398bd964ecbc0ec0
Ingi Thor Sigurdsson <ingisigurds@gmail.com>
2018-12-11 15:06:49 +0000

    Added the logic to upload files

--- thinkback/views/upload.py
+++ thinkback/views/upload.py
@@ -3,15 +3,14 @@ from flask import current_app as app
 from werkzeug.utils import secure_filename
 from flask import Flask, Blueprint, render_template, request, flash, redirect, url_for
 
-upload = Blueprint('/upload', __name__)
 
 UPLOAD_FOLDER = './uploads/python'
 ALLOWED_EXTENSIONS = set(['py'])
 
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+upload = Blueprint('/upload', __name__)
 
-@app.route('/upload', methods=['POST'])
-def upload_file():
+@upload.route('/upload/<problem_name>', methods=['POST'])
+def upload_file(problem_name):
 	if request.method == 'POST':
 		# Check if the post request has a file in it
 		if 'file' not in request.files:
@@ -23,7 +22,10 @@ def upload_file():
 			return redirect(request.url)
 		if file and allowed_file(file.filename):
 			filename = secure_filename(file.filename)
-			file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+			path_to_problem = os.path.join(app.config['UPLOAD_FOLDER'], ''.join(problem_name))
+			if not os.path.exists(path_to_problem):
+				os.makedirs(path_to_problem)
+			file.save(os.path.join(path_to_problem, filename))
 			return url_for('get_active_assignments')
 	return ""
 
