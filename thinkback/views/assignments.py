f62a8a4b977fc1228aa6692cbd3980c50c21e61b
Ingi Thor Sigurdsson <ingisigurds@gmail.com>
2018-12-11 09:57:47 +0000

    Assignments.py is now routing correctly for each assignemnt, and for each problem

--- thinkback/views/assignments.py
+++ thinkback/views/assignments.py
@@ -1,4 +1,4 @@
-	# thinkback/views/assignments.py
+# thinkback/views/assignments.py
 
 from flask import Blueprint, render_template
 from ..models import Assignment
@@ -10,9 +10,11 @@ assignment_blueprint = Blueprint('/assignments', __name__)
 assignment_list = []
 problem = Assignment("Basics in python")
 problem.create_problem("Hello world", "Simply print out hello world")
-problem.create_problem("If statements", "Do something simple with if statements")
+problem.create_problem(
+    "If statements", "Do something simple with if statements")
 problem.create_problem("While loops", "Do something simple with while loops")
-problem.create_problem("AI", "Use machine learning techniques that you learned to create an AI to determine the probabilty of human destructoin")
+problem.create_problem(
+    "AI", "Use machine learning techniques that you learned to create an AI to determine the probabilty of human destructoin")
 problem2 = Assignment("Advance mathematics in python")
 assignment_list.append(problem)
 assignment_list.append(problem2)
@@ -25,15 +27,24 @@ def get_active_assignments():
 
 @assignment_blueprint.route('/active_assignments/<assignment>', methods=['GET'])
 def get_problems(assignment):
-	the_assignment = get_problems_with_assignment(assignment)
-	return render_template('problems.html', assignment=the_assignment)
+    the_assignment = get_problems_with_assignment(assignment)
+    return render_template('problems.html', assignment=the_assignment)
 
 
 @assignment_blueprint.route('/active_assignments/<assignment>/<problem>', methods=['GET'])
-def get_assignment_problem(assignment, problem):
+def get_problem_details(assignment, problem):
+    problem = get_single_problem(assignment, problem)
     return render_template('problem.html', problem=problem)
 
+
 def get_problems_with_assignment(name):
-	for assignment in assignment_list:
-		if assignment.name == name:
-			return assignment
+    for assignment in assignment_list:
+        if assignment.name == name:
+            return assignment
+
+
+def get_single_problem(assignment, problem_name):
+    assignment = get_problems_with_assignment(assignment)
+    for problem in assignment.problem_list:
+        if problem.name == problem_name:
+            return problem
