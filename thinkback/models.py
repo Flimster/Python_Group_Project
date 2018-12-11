c273c0c1ca5bd150ea89a7382fad7900bf2b56d4
Ingi Thor Sigurdsson <ingisigurds@gmail.com>
2018-12-11 18:52:25 +0000

    Added a method that converts models to json objects

--- thinkback/models.py
+++ thinkback/models.py
@@ -1,4 +1,5 @@
 import uuid
+import json
 
 class Assignment:
 	def __init__(self, name):
@@ -6,6 +7,9 @@ class Assignment:
 		self.problem_list = []
 		self.problem_count = 0
 		self.active = True
+
+	def toJson(self):
+		 return json.dumps(self, default=lambda o: o.__dict__, sort_keys=True, indent=4)
 
 	def create_problem(self, name, description):
 		self.problem_list.append(Problem(name, description))
@@ -17,3 +21,6 @@ class Problem:
 		self.name = name
 		self.description = description
 		self.id = str(uuid.uuid4())
+	
+	def toJson(self):
+		 return json.dumps(self, default=lambda o: o.__dict__, sort_keys=True, indent=4)
