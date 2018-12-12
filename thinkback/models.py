fde51f23e2dc68f5f53ee497a7fa410670852521
Ingi Thor Sigurdsson <ingisigurds@gmail.com>
2018-12-12 11:18:45 +0000

    Added so that problems must have a function name

--- thinkback/models.py
+++ thinkback/models.py
@@ -10,15 +10,16 @@ class Assignment:
 	def toJson(self):
 		 return {"name": self.name, "problem_list": self.problem_list, "active": self.active }
 
-	def create_problem(self, name, desc):
-		self.problem_list.append(Problem(name, desc))
+	def create_problem(self, name, desc, function_name):
+		self.problem_list.append(Problem(name, desc, function_name))
 
 
 class Problem:
-	def __init__(self, name, desc):
+	def __init__(self, name, desc, function_name):
 		self.name = name
 		self.desc = desc
 		self.id = str(uuid.uuid4())
+		self.function_name = function_name
 	
 	def toJson(self):
 		 return {"name": self.name, "description": self.desc, "id": self.id}
