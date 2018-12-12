8b0d907841162070d9fbe8a00815f9b38f059faa
Ingi Thor Sigurdsson <ingisigurds@gmail.com>
2018-12-12 15:20:47 +0000

    Refactores assignments.py and models.py moving functionality from assignments.py to models.py and creating a class called FileUploaded

--- thinkback/models.py
+++ thinkback/models.py
@@ -1,5 +1,9 @@
+import os
 import uuid
 import json
+import importlib
+from flask import current_app as app
+
 
 class Assignment:
 	def __init__(self, name, description, active):
@@ -8,11 +12,10 @@ class Assignment:
 		self.active = active
 
 	def toJson(self):
-		 return {"name": self.name, "problem_list": self.problem_list, "active": self.active }
+		return {"name": self.name, "problem_list": self.problem_list, "active": self.active}
 
 	def create_problem(self, name, desc):
 		self.problem_list.append(Problem(name, desc, "sum_two", self.name))
-
 
 
 class Problem:
@@ -22,10 +25,36 @@ class Problem:
 		self.assignment_name = assignment_name
 		self.function_name = function_name
 		self.id = str(uuid.uuid4())
-	
-	def toJson(self):
-		 return {"name": self.name, "description": self.desc, "id": self.id}
 
+	def toJson(self):
+		return {"name": self.name, "description": self.desc, "id": self.id}
+
+
+class UploadedFile:
+	def __init__(self, file):
+		self.file = file
+		self._allowed_extensions = set(['py'])
+
+	def is_empty(self):
+		if self.file.filename == '':
+			return True
+		return False
+
+	def is_allowed(self):
+		return '.' in self.file.filename and \
+			self.file.filename.rsplit('.', 1)[1].lower() in self._allowed_extensions
+
+	def create_file_path(self, problem_id):
+		return os.path.join(app.config['UPLOAD_FOLDER'], ''.join(problem_id))
+
+	def save_file_to_path(self, path, secure_filename):
+		self.file.save(os.path.join(path, secure_filename))
+		self.file.save(os.path.join(path, '__init__.py'))
+
+	def get_file_module(self, module_path, filename):
+		filename = filename.split('.')
+		module = importlib.import_module('.{}'.format(filename[0]), package=module_path)
+		return module
 
 class TestCase:
 	def __init__(self, problem_id):
--- thinkback/views/assignments.py
+++ thinkback/views/assignments.py
@@ -2,7 +2,7 @@
 
 import os
 import importlib
-from ..models import Assignment
+from ..models import Assignment, UploadedFile
 from flask import current_app as app
 from werkzeug.utils import secure_filename
 from flask import Blueprint, render_template, request, redirect, flash, url_for
@@ -15,18 +15,24 @@ assignment_blueprint = Blueprint('/assignments', __name__)
 # TODO: Delete this
 # This is used for testing the various routes
 assignment_list = []
-assignment1 = Assignment("Basics in python", "People search for this Lorem ipsum dummy copy text using all kinds of names, such as Lorem ipsum, lorem ipsum dolor sit amet, Lorem, dummy text, loren ipsum (yes, spelled wrong)", True)
+assignment1 = Assignment(
+    "Basics in python", "People search for this Lorem ipsum dummy copy text using all kinds of names, such as Lorem ipsum, lorem ipsum dolor sit amet, Lorem, dummy text, loren ipsum (yes, spelled wrong)", True)
 assignment1.create_problem("Hello world", "Simply print out hello world")
-assignment1.create_problem("If statements", "Do something simple with if statements")
-assignment1.create_problem("While loops", "Do something simple with while loops")
-assignment1.create_problem("AI", "Use machine learning techniques that you learned to create an AI to determine the probabilty of human destructoin")
-assignment2 = Assignment("Advance mathematics in python", "I’ve seen several versions of this Lorem ipsum text on the Internet, with various changes; however, this Lorem Ipsum dummy copy text doesn’t have any odd text sneaked in as I’ve seen in some of the others.", True)
+assignment1.create_problem(
+    "If statements", "Do something simple with if statements")
+assignment1.create_problem(
+    "While loops", "Do something simple with while loops")
+assignment1.create_problem(
+    "AI", "Use machine learning techniques that you learned to create an AI to determine the probabilty of human destructoin")
+assignment2 = Assignment("Advance mathematics in python",
+                         "I’ve seen several versions of this Lorem ipsum text on the Internet, with various changes; however, this Lorem Ipsum dummy copy text doesn’t have any odd text sneaked in as I’ve seen in some of the others.", True)
 assignment2.create_problem("TYLER", "Try to be awesome like him")
 assignment2.create_problem("INGI", "The cool kid")
 assignment_list.append(assignment1)
 assignment_list.append(assignment2)
-#Testing for past assigments
-assignment3 = Assignment("Number Lists", "There is an unformatted plain text (.txt) version to either copy/paste or to save the .txt file to your computer. Note: it’s unformatted and will stretch across your screen. ",False)
+# Testing for past assigments
+assignment3 = Assignment(
+    "Number Lists", "There is an unformatted plain text (.txt) version to either copy/paste or to save the .txt file to your computer. Note: it’s unformatted and will stretch across your screen. ", False)
 assignment3.create_problem("Create an asc. number list", "33% of grade")
 assignment3.create_problem("Create a desc. number list", "33% of grade")
 assignment3.create_problem(
@@ -35,13 +41,15 @@ assignment_list.append(assignment3)
 
 # Gives the right path to the 'link' coming in, and also gets a right question list
 
+
 @assignment_blueprint.route('/<link>', methods=['GET'])
 def get_assignment_path(link):
-    if link == 'active_assignments': 
+    if link == 'active_assignments':
         return render_template('assignments.html', assignment_list=get_assignment_list(True))
-    if link == 'past_assignments': 
+    if link == 'past_assignments':
         return render_template('assignments.html', assignment_list=get_assignment_list(False))
     return "Something went wrong"
+
 
 @assignment_blueprint.route('/<link>/<problem>', methods=['GET'])
 def get_problems(link, problem):
@@ -54,28 +62,29 @@ def upload_file(link, problem_id):
     if request.method == 'POST':
         # Check if the post request has a file in it
         file = request.files['file']
-
-        if file.filename == '':
+        file_uploaded = UploadedFile(file)
+        if file_uploaded.is_empty():
             flash("Something went wrong")
             return redirect(request.url)
 
-        if file and allowed_file(file.filename):
-            filename = secure_filename(file.filename)
-            path = create_file_path(problem_id)
+        if file_uploaded.file and file_uploaded.is_allowed():
+            filename = secure_filename(file_uploaded.file.filename)
+            path = file_uploaded.create_file_path(problem_id)
 
             if not os.path.exists(path):
                 os.makedirs(path)
 
-            save_file_to_path(path, file, filename)
-
+            file_uploaded.save_file_to_path(path, filename)
             module_path = '.uploads.{}'.format(problem_id)
-            problem_module = get_file_module(module_path, filename)
+            problem_module = file_uploaded.get_file_module(module_path, filename)
             function_name = get_problem_info(problem_id).function_name
             problem_function = getattr(problem_module, function_name)
             return redirect(request.url)
 
+
     # TODO: Return error that something went wrong
     return ""
+
 
 def get_assignment_list(status):
     filtered_assignment_list = []
@@ -84,35 +93,16 @@ def get_assignment_list(status):
             filtered_assignment_list.append(assignment)
     return filtered_assignment_list
 
+
 def get_problem_info(problem_id):
     assignment = get_assignment_by_problem_id(problem_id)
     for problem in assignment.problem_list:
         if problem.id == problem_id:
             return problem
 
+
 def get_assignment_by_problem_id(problem_id):
     for assigment in assignment_list:
         for problem in assigment.problem_list:
             if problem.id == problem_id:
                 return assigment
-
-def has_file(request):
-    if 'file' not in request.files:
-        return redirect(request.url)
-def allowed_file(filename):
-    return '.' in filename and \
-           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
-
-def create_file_path(problem_id):
-    path = os.path.join(app.config['UPLOAD_FOLDER'], ''.join(problem_id))
-    return path
-
-def save_file_to_path(path, file, secure_filename):
-    file.save(os.path.join(path, secure_filename))
-    file.save(os.path.join(path, '__init__.py'))
-
-def get_file_module(module_path, filename):
-    filename = filename.split('.')
-    module = importlib.import_module(
-        '.{}'.format(filename[0]), package=module_path)
-    return module
