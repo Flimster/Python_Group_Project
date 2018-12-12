575d3d501296d908de1a0a556260c66241cfd7c4
Ingi Thor Sigurdsson <ingisigurds@gmail.com>
2018-12-12 09:27:15 +0000

    Changed the structure of the models

--- thinkback/models.py
+++ thinkback/models.py
@@ -5,7 +5,7 @@ class Assignment:
 	def __init__(self, name, active):
 		self.name = name
 		self.problem_list = []
-		self.active = True
+		self.active = active
 
 	def toJson(self):
 		 return {"name": self.name, "problem_list": self.problem_list, "active": self.active }
@@ -17,8 +17,8 @@ class Assignment:
 class Problem:
 	def __init__(self, name, desc):
 		self.name = name
-		self.description = desc
+		self.desc = desc
 		self.id = str(uuid.uuid4())
 	
 	def toJson(self):
-		 return {"name": self.name, "description": self.description, "id": self.id}
+		 return {"name": self.name, "description": self.desc, "id": self.id}
