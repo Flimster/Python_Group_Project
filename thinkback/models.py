90ef438913b5a4e9c0faf2421fc0d233146bbf21
Ingi Thor Sigurdsson <ingisigurds@gmail.com>
2018-12-13 09:33:28 +0000

    Refactored models to compenaste for the DB

--- thinkback/models.py
+++ thinkback/models.py
@@ -3,25 +3,14 @@ import json
 
 class Assignment:
 	def __init__(self, name, description, active):
+		self.id = str(uuid.uuid4())
 		self.name = name
-		self.problem_list = []
 		self.active = active
-
-	def toJson(self):
-		 return {"name": self.name, "problem_list": self.problem_list, "active": self.active }
-
-	def create_problem(self, name, desc):
-		self.problem_list.append(Problem(name, desc, "sum_two", self.name))
-
-
 
 class Problem:
 	def __init__(self, name, desc, function_name, assignment_name):
+		self.id = str(uuid.uuid4())
 		self.name = name
 		self.desc = desc
 		self.assignment_name = assignment_name
 		self.function_name = function_name
-		self.id = str(uuid.uuid4())
-	
-	def toJson(self):
-		 return {"name": self.name, "description": self.desc, "id": self.id}
