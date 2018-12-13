80cb4735898c08a9fee95fd6da79d39560961468
Ingi Thor Sigurdsson <ingisigurds@gmail.com>
2018-12-13 16:58:00 +0000

    Edited thinkback.py

--- thinkback/thinkback.py
+++ thinkback/thinkback.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request, session, g, redirect, url_for, abort, \
     render_template, flash
 import os
-from .views import about, assignments, database
+from .views import about, assignments
 import sqlite3
 
 UPLOAD_FOLDER = './uploads'
@@ -12,7 +12,6 @@ app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 app.register_blueprint(about.about_blueprint)
 app.register_blueprint(assignments.assignment_blueprint)
-app.register_blueprint(database.database_blueprint)
 
 app.config.from_object(__name__)  # load config from this file , flaskr.py
 # Load default config and override config from an environment variable
@@ -26,18 +25,50 @@ app.config.update(dict(
 app.config.from_envvar('FLASKR_SETTINGS', silent=True)
 
 # Database command line tools
+
+
 @app.cli.command('dropdb')
 def drop_db_command():
     print("Dropping database.")
-    database.drop_db()
+    drop_db()
     print("Dropped database.")
 
 
 @app.cli.command('initdb')
 def initdb_command():
     print("""Initializes the database.""")
-    database.init_db()
+    init_db()
     print('Initialized the database.')
+
+
+def drop_db():
+    db = get_db()
+    with app.open_resource('drop.sql', mode='r') as f:
+        db.cursor().executescript(f.read())
+    db.commit()
+
+
+def init_db():
+    db = get_db()
+    with app.open_resource('schema.sql', mode='r') as f:
+        db.cursor().executescript(f.read())
+    db.commit()
+
+
+def get_db():
+    """Opens a new database connection if there is none yet for the
+        current application context.
+        """
+    if not hasattr(g, 'flaskr.db'):
+        g.sqlite_db = connect_db()
+    return g.sqlite_db
+
+
+def connect_db():
+    """Connects to the specific database."""
+    rv = sqlite3.connect(app.config['DATABASE'])
+    rv.row_factory = sqlite3.Row
+    return rv
 
 
 @app.teardown_appcontext
