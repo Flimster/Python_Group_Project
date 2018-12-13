46206ef1c697a98a22c576f9f7b87b67fe1a9441
Ingi Thor Sigurdsson <ingisigurds@gmail.com>
2018-12-13 16:24:01 +0000

    Moved database funcitonality

--- thinkback/thinkback.py
+++ thinkback/thinkback.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request, session, g, redirect, url_for, abort, \
-	render_template, flash
+    render_template, flash
 import os
-from .views import about, assignments
+from .views import about, assignments, database
 import sqlite3
 
 UPLOAD_FOLDER = './uploads'
@@ -12,72 +12,47 @@ app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 app.register_blueprint(about.about_blueprint)
 app.register_blueprint(assignments.assignment_blueprint)
+app.register_blueprint(database.database_blueprint)
 
 app.config.from_object(__name__)  # load config from this file , flaskr.py
 # Load default config and override config from an environment variable
 
 app.config.update(dict(
-	DATABASE=os.path.join(app.root_path, 'flaskr.db'),
-	SECRET_KEY='super secret key',
-	USERNAME='admin',
-	PASSWORD='default'
+    DATABASE=os.path.join(app.root_path, 'flaskr.db'),
+    SECRET_KEY='super secret key',
+    USERNAME='admin',
+    PASSWORD='default'
 ))
-
 app.config.from_envvar('FLASKR_SETTINGS', silent=True)
 
+# Database command line tools
+@app.cli.command('dropdb')
+def drop_db_command():
+    print("Dropping database.")
+    database.drop_db()
+    print("Dropped database.")
 
-def connect_db():
-	print("""Connects to the specific database.""")
-	rv = sqlite3.connect(app.config['DATABASE'])
-	rv.row_factory = sqlite3.Row
-	return rv
 
-
-def get_db():
-	print("""Opens a new database connection if there is none yet for the
-	current application context.
-	""")
-	if not hasattr(g, 'flaskr.db'):
-		g.sqlite_db = connect_db()
-	return g.sqlite_db
+@app.cli.command('initdb')
+def initdb_command():
+    print("""Initializes the database.""")
+    database.init_db()
+    print('Initialized the database.')
 
 
 @app.teardown_appcontext
 def close_db(error):
-	print("""Closes the database again at the end of the request.""")
-	if hasattr(g, 'flaskr.db'):
-		g.sqlite_db.close()
-
-@app.cli.command('initdb')
-def initdb_command():
-	print("""Initializes the database.""")
-	init_db()
-	print('Initialized the database.')
-
-def init_db():
-	db = get_db()
-	with app.open_resource('schema.sql', mode='r') as f:
-		db.cursor().executescript(f.read())
-	db.commit()
-
-@app.cli.command('dropdb')
-def drop_db_command():
-	print("Dropping database.")
-	drop_db()
-	print("Dropped database.")
-
-def drop_db():
-	db = get_db()
-	with app.open_resource('drop.sql', mode='r') as f:
-		db.cursor().executescript(f.read())
-	db.commit()
+    print("""Closes the database again at the end of the request.""")
+    if hasattr(g, 'flaskr.db'):
+        g.sqlite_db.close()
 
 
 @app.route('/index')
 @app.route('/')
 def index():
-	return render_template('/index.html')
+    return render_template('/index.html')
+
 
 if __name__ == '__main__':
-	app.config['SESSION_TYPE'] = 'filesystem'
-	app.run(debug=True)
+    app.config['SESSION_TYPE'] = 'filesystem'
+    app.run(debug=True)
