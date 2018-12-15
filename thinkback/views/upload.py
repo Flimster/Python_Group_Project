910d7bc65afa8b1b9295a025216064d149060338
Ingi Thor Sigurdsson <ingisigurds@gmail.com>
2018-12-15 11:33:59 +0000

    Added some error handling  for the uplaod code

--- thinkback/views/upload.py
+++ thinkback/views/upload.py
@@ -14,43 +14,47 @@ upload_blueprint = Blueprint('upload', __name__)
 
 @upload_blueprint.route('/<link>/<problem_id>', methods=['POST'])
 def upload_file(link, problem_id):
-    if request.method == 'POST':
-        # Check if the post request has a file in it
-        file = request.files['file']
+	if request.method == 'POST':
+		problem = database.get_single_problem(problem_id)
 
-        submitted_file = ProblemModule(file)
-        if submitted_file.is_empty():
-            flash("Something went wrong")
-            return redirect(request.url)
+		if 'file' not in request.files:
+			flash('No file part')
+			return redirect(request.url)
 
-        if submitted_file.file and submitted_file.is_allowed():
-            filename = secure_filename(file.filename)
-            path = submitted_file.create_file_path(problem_id)
+		file = request.files['file']
+		submitted_file = ProblemModule(file)
+		
+		if file.filename == '':
+			flash("No selected file")
+			return redirect(request.url)
 
-            if not os.path.exists(path):
-                os.makedirs(path)
 
-            submitted_file.save_files_to_path(path, filename)
+		elif file and submitted_file.is_allowed():
+			filename = secure_filename(file.filename)
+			path = submitted_file.create_file_path(problem_id)
 
-            module_path = '.uploads.{}'.format(problem_id)
-            solution_path = '.impl.{}'.format(problem_id)
+			if not os.path.exists(path):
+				os.makedirs(path)
 
-            problem_module = submitted_file.get_file_module(
-                module_path, filename)
-            function_name = database.get_function_name(problem_id)
-            # Try to import the fuction needed for the problem
-            try:
-                problem_function = getattr(problem_module, function_name)
-                solution = importlib.import_module(
-                    '.correct', package=solution_path)
-                tmp = solution.Solution(problem_function)
-                results = tmp.run_tests()
-                problem = database.get_single_problem(problem_id)
-                return render_template('problem.html', link=link, problem=problem, results=results)
-            except AttributeError:
-                # If the function did not exists remove the file
-                os.remove(os.path.join(path, filename))
-            return redirect(request.url)
+			submitted_file.save_files_to_path(path, filename)
 
-    # TODO: Return error that something went wrong
-    return ""
+			module_path = '.uploads.{}'.format(problem_id)
+			solution_path = '.impl.{}'.format(problem_id)
+
+			problem_module = submitted_file.get_file_module(
+				module_path, filename)
+			function_name = database.get_function_name(problem_id)
+			# Try to import the fuction needed for the problem
+			try:
+				problem_function = getattr(problem_module, function_name)
+				solution = importlib.import_module(
+					'.correct', package=solution_path)
+				tmp = solution.Solution(problem_function)
+				results = tmp.run_tests()
+				problem = database.get_single_problem(problem_id)
+			except AttributeError:
+				# If the function did not exists remove the file
+				flash("Could not import the correct file name")
+				os.remove(os.path.join(path, filename))
+
+	return render_template('problem.html', link=link, problem=problem, results=results)
