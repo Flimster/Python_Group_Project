9a087c1024c887f207f61242b5318f586b287b5f
Ingi Thor Sigurdsson <ingisigurds@gmail.com>
2018-12-15 15:56:42 +0000

    Fixed routing

--- thinkback/views/assignments.py
+++ thinkback/views/assignments.py
@@ -10,11 +10,12 @@ assignment_blueprint = Blueprint('/assignments', __name__)
 def get_assignment_path(link):
     assignment_list = database.get_assignments_with_problems()
 
+
     if link == 'active_assignments':
         return render_template('assignments.html', assignment_list=filter_assignments(assignment_list, 1), flag=True)
     elif link == 'past_assignments':
         return render_template('assignments.html', assignment_list=filter_assignments(assignment_list, 0), flag=False)
-    elif link.isdigit():
+    elif link.isdigit() and int(link) <= len(assignment_list):
         return render_template('createassignment.html', assignment_id=link)
     return render_template("404.html")
 
