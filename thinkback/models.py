1e33b41b1aff6c2174b9d925e43042abbb82e28b
Ingi Thor Sigurdsson <ingisigurds@gmail.com>
2018-12-16 12:32:52 +0000

    Removed unneccessary imports in the models file

--- thinkback/models.py
+++ thinkback/models.py
@@ -1,18 +1,9 @@
-import os
-import importlib
-import unittest
-from flask import current_app as app
-
-
 class Assignment:
 	def __init__(self, id, name, active):
 		self.id = id
 		self.name = name
 		self.active = active
 		self.problem_list = []
-
-	def __str__(self):
-		return '{} {} {}'.format(self.id, self.name, self.active)
 
 class Problem:
 	def __init__(self, id, assignment_id, name, desc, function):
